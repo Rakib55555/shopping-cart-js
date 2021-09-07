0337a2c4b83845de65abf47043e3ba1be4537589
Rakib55555 <rakibulislamrakibprince@gmail.com>
2021-09-07 19:24:20 +0600

    project complete

--- app.js
+++ app.js
@@ -1,69 +1,51 @@
-//casing section//
-function updateCaseNumber(incrising){
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
-    
-    if(incrising == true){
-        caseNumber = parseInt(caseNumber) + 1;
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if(caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-    //update case price
-    const casePrice = document.getElementById('case-price');
-    casePrice.innerText = caseNumber * 59;
-};
-//case plus item button//
-document.getElementById('case-plus').addEventListener('click',function(){
-    updateCaseNumber(true)
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    // calculate total
     calculateTotal();
-});
+}
 
-document.getElementById('case-minus').addEventListener('click',function(){
-    updateCaseNumber(false)
-    calculateTotal();
-});
-function getInputValue(product){
-    const productNumber = document.getElementById(product + '-number');
-    const productInput = parseInt(productNumber.value);
-    return productInput;
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
 }
 
 function calculateTotal() {
-
-    const iphoneTotal = getInputValue('phone') * 1219;
+    const phoneTotal = getInputValue('phone') * 1219;
     const caseTotal = getInputValue('case') * 59;
-    const subTotal = iphoneTotal + caseTotal;
-    console.log(subTotal);
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
 }
 
-// iphone section//
-function updateIphoneNumber(isIncrise){
-    const iphoneInput = document.getElementById('iphone-number');
-    let iphoneNumber = iphoneInput.value;
-    if(isIncrise == true){
-        iphoneNumber = parseInt(iphoneNumber) + 1;
-    }
-    else if(iphoneNumber > 0){
-        iphoneNumber = parseInt(iphoneNumber) - 1;
-    }
-    iphoneInput.value = iphoneNumber;
-
-    //update iphone price
-    const iphonePrice = document.getElementById('iphone-price');
-    iphonePrice.innerText = iphoneNumber * 1219;
-};
-
-//iphone pluse Button
-document.getElementById('iphone-plus').addEventListener('click', function(){
-    updateIphoneNumber(true);
-    calculateTotal();
-    
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
 });
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+})
 
-//iphone minus button
-document.getElementById('iphone-minus').addEventListener('click', function(){
-    updateIphoneNumber(false);
-    calculateTotal();
+
+// handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
 });
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+})
